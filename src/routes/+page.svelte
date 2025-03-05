0cc6048b266625fe0eb29ead8e4f05b9c291ce6f
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2025-03-05 23:58:30 +0000

    i'm seriously done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+	:global(*) {
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 12pt;
+	}
+</style>
