69be5a18a37dfab53c8be4516ab178fe7378815a
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2025-03-05 23:57:55 +0000

    i'm done for now

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,13 +1,34 @@
 <svelte:head>
-	<title>IT WORKED OMG!</title>
+	<title>I'm tired...</title>
 </svelte:head>
 
-<p>THANK GOD IT FINALLY WORKED!</p>
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	let finalCanvas: HTMLCanvasElement;
+
+	onMount(() => {
+		let ctx : any = finalCanvas.getContext("2d");
+		ctx.strokeStyle = "#0000FF";
+		ctx.moveTo(0,0);
+		ctx.lineTo(200,100);
+		ctx.stroke();
+	});
+</script>
 
 
+
+<main>
+	<h3>Hello. How are you?</h3>
+	<h2>I am under the water.</h2>
+	<h2>Please help me, here too much raining,</h2>
+	<h1>Woooooooo!</h1>
+	
+	<canvas bind:this={ finalCanvas } width="200" height="100"></canvas>
+</main>
 
 <style>
-	* {
-		font-family: Arial, Helvetica, sans-serif;
+	canvas {
+		border: 1px solid black;
 	}
 </style>
