f2d862519a25e07f71f39b0be9f176244aaa60fb
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2025-03-05 21:40:57 +0000

    one more time

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>IT WORKED OMG!</title>
+</svelte:head>
+
+<p>THANK GOD IT FINALLY WORKED!</p>
+
+
+
+<style>
+	* {
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
