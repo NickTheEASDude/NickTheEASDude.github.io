580e0c2d1acd10cb86dc77413e60bfce8938dada
blt-sandwich-8704 <88751815+NickTheEASDude@users.noreply.github.com>
2025-05-13 19:22:58 +0000

    Finish things up.

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,20 +3,38 @@
 </svelte:head>
 
 <script lang="ts">
-	import frisbee from "$imports/frisbee.png";
+	import frisbee from "$lib/assets/frisbee.png";
+	import allcode from "$lib/assets/allcode.mp4";
+	import raspi from "$lib/assets/raspi.mp4";
+	
 </script>
 <main class="outlined">
+	<h2>Here is a frisbee I spent a month designing.</h2>
 	<div class="edges">
 		<div class="middle">
-			<div class="bg edges">
-				<img src={ frisbee } alt="FRISBEEEEEEEEEEEEEEEEE"/>
-			</div>
+			<img src={ frisbee } alt="FRISBEEEEEEEEEEEEEEEEE"/>
 		</div>
 	</div>
 	<br style="height: 25px;"/>
+	<hr/>
+	<h2>A raspberry pi I set up as well &#40this took forever...&#41</h2>
 	<div class="edges">
 		<div class="middle">
-			<div class="bg edges" style=" width: 900px; height: 600px;"></div>
+			<video muted autoplay loop width=720px height=405px>
+				<source src={ raspi } type="video/mp4"/>
+				No video support in browser.
+			</video>
+		</div>
+	</div>
+	<br style="height: 25px;"/>
+	<hr/>
+	<h2>Finally, a bluetooth robot called Formula AllCode. This was a bit of a pain, because of the way it connects to bluetooth on Linux.</h2>
+	<div class="edges">
+		<div class="middle">
+			<video muted autoplay loop width=720px height=405px>
+				<source src={ allcode } type="video/mp4"/>
+				No video support in browser.
+			</video>
 		</div>
 	</div>
 </main>
@@ -31,8 +49,10 @@
 	}
 	div.middle {
 		border: 3px solid black;
+		padding: 0px;
 	}
-	div.bg {
-		background-image: url("brick.png");
+
+	video, img {
+		display: block;
 	}
 </style>
